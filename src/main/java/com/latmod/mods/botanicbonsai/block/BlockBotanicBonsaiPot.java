9ba64982138a5476191735849af698b4aef5f87a
LatvianModder <latvianmodder@gmail.com>
2019-02-03 12:01:23 +0200

    Fixed pot not dropping correct item

--- src/main/java/com/latmod/mods/botanicbonsai/block/BlockBotanicBonsaiPot.java
+++ src/main/java/com/latmod/mods/botanicbonsai/block/BlockBotanicBonsaiPot.java
@@ -4,11 +4,18 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.dave.bonsaitrees.block.BlockBonsaiPot;
+import org.dave.bonsaitrees.tile.TileBonsaiPot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author LatvianModder
@@ -55,5 +62,52 @@ public class BlockBotanicBonsaiPot extends BlockBonsaiPot
 	public IBlockState getStateFromMeta(int meta)
 	{
 		return getDefaultState();
+	}
+
+	@Override
+	public void onBlockHarvested(World world, BlockPos pos, IBlockState state, EntityPlayer player)
+	{
+		if (!world.isRemote)
+		{
+			if (!player.isCreative())
+			{
+				if (world.getTileEntity(pos) instanceof TileBonsaiPot)
+				{
+					List<ItemStack> drops = new ArrayList<>();
+					TileBonsaiPot pot = (TileBonsaiPot) world.getTileEntity(pos);
+
+					if (pot.hasSapling())
+					{
+						if (pot.isHarvestable())
+						{
+							drops.addAll(pot.getRandomizedDrops());
+						}
+
+						drops.add(pot.getSapling());
+					}
+
+					if (pot.hasSoil())
+					{
+						drops.add(pot.getSoilStack());
+					}
+
+					ItemStack potStack = new ItemStack(this, 1, this.getMetaFromState(state));
+
+					if (pot.getColor() != null)
+					{
+						NBTTagCompound potNbt = new NBTTagCompound();
+						potNbt.setInteger("color", pot.getColor().getMetadata());
+						potStack.setTagCompound(potNbt);
+					}
+
+					drops.add(potStack);
+
+					for (ItemStack drop : drops)
+					{
+						spawnAsEntity(world, pos, drop);
+					}
+				}
+			}
+		}
 	}
 }
